f367c2c2c9e94b6c5f0c3103a4ec940134884e38
Yash Khalkar <yashmk2004@gmail.com>
2025-10-07 11:25:05 +0530

    user route created

--- be/src/app.js
+++ be/src/app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { SIZE_LIMIT } from "./constants";
+import { SIZE_LIMIT } from "./constants.js";
+import userRouter from "./routes/user.routes.js";
 
 const app = express();
 
@@ -16,5 +17,7 @@ app.use(express.json({ limit: SIZE_LIMIT }));
 app.use(express.urlencoded({ extended: true, limit: SIZE_LIMIT }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+app.use("/api/v1/users", userRouter);
 
 export { app };
